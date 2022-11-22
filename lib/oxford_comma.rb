e40fbe9141b5b6cf445a99ae63cd81a9f752de16
Algernon Soriano <alger.soriano@gmail.com>
2022-11-22 21:42:21 +1000

    oxford_comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    last_word = ""
+    x = array.length
+    if x < 3
+        joined = array.join(" and ")
+    elsif x >= 3
+        last_word = ", and #{array[x-1]}"
+        array.pop
+        joined = array.join(", ")
+    end
+    return "#{joined}#{last_word}"
+        
 end
+
+array = ["a", "b", "c", "d"]
+puts oxford_comma(array)
